5913008ee4ab829ea4fe7aa72315d2cb8c1e5616
Robert M. Verano <robert.verano@bren-inc.com>
2024-11-05 11:27:52 +0800

    add other modules

---
+++ src/app/reusableComponent/alert/alert.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-alert',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './alert.component.html',
+  styleUrl: './alert.component.css',
+})
+export class AlertComponent {
+  @Input() alertType: string = '';
+  @Input() alertMessage: string = '';
+
+  @Input() dynamicClass: boolean = true;
+}
